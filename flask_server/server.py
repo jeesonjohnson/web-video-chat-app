abf52df95ceec7d455234f8c83eb5ebcb5dd54f7
TheRuneWizard <znau19@gmail.com>
2021-02-27 17:40:58 +0000

    Basic form done

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,18 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
    if request.method == 'POST':
-      pass
+      content = request.form['content']
+      return content
    else: 
       return render_template('index.html')
-
-
-
-@app.route('/upload')
-def upload():
-   return render_template('upload.html')
 
 
 if __name__ == '__main__':
