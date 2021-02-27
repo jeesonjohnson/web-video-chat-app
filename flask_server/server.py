ad51ce527e37002a033f608fb1a37bb0cf20b11c
JoshSPH <70639288+JoshSPH@users.noreply.github.com>
2021-02-27 17:26:09 +0000

    Added tempalte

--- flask_server/server.py
+++ flask_server/server.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello_world():
    return render_template('index.html')
 
+
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+
+
 if __name__ == '__main__':
    app.run()
