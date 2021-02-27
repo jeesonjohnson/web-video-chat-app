7f291c91acca6713eb3d0cc6a4268d19587b67da
TheRuneWizard <znau19@gmail.com>
2021-02-27 17:26:39 +0000

    Form stuff 1

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():
+   if request.method == 'POST':
+      pass
+   else: 
+      return render_template('index.html')
+
 
 if __name__ == '__main__':
    app.run()
